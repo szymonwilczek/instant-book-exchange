8da9fb9a8b420e17f8e6574de459667a4dbc38d4
nitruseq <rafxoxd@gmail.com>
2026-01-27 12:43:52 +0100

    remove deprecated matchMedia methods from mock
    Remove deprecated addListener/removeListener from useMediaQuery mock. Using modern addEventListener/removeEventListener instead.

--- tests/useMediaQuery.test.ts
+++ tests/useMediaQuery.test.ts
@@ -10,8 +10,6 @@ const createMatchMedia = (matches: boolean) => {
         matches,
         media: query,
         onchange: null,
-        addListener: jest.fn(), // deprecated
-        removeListener: jest.fn(), // deprecated
         addEventListener: jest.fn(),
         removeEventListener: jest.fn(),
         dispatchEvent: jest.fn(),
